5f4526ca234f40e678587438f58182b2ef464ea2
hyeongcheol <jangdol012@naver.com>
2024-02-14 15:52:07 +0900

    first commit

---
+++ src/main/java/com/green/bloom/GitBashTestApplication.java
@@ -0,0 +1,13 @@
+package com.green.bloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBashTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBashTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/bloom/GitBashTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.bloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBashTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
